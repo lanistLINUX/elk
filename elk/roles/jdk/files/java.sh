d658b0c3361cf9308a59942c5052a3ac4b47c191
lanistLINUX <64882775+lanistLINUX@users.noreply.github.com>
2020-07-08 17:40:35 +0800

    Add files via upload

---
+++ elk/roles/jdk/files/java.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/usr/local/jdk1.8.0_161
+export JRE_HOME=/usr/local/jdk1.8.0_161/jre
+export CLASSPATH=.:$CLASSPATH:$JAVA_HOME/lib:$JRE_HOME/lib
+export PATH=$PATH:$JAVA_HOME/bin:$JRE_HOME/bin
